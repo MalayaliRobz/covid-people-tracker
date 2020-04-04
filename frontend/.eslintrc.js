0723dcb92102320ccc445025dbb6a143d66e318b
Robin Philip Thomas <robin.thomas@freshworks.com>
2020-04-04 15:07:29 +0530

    typescript fix

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,56 +1,62 @@
-'use strict';
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
-  },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
-  env: {
-    browser: true
-  },
-  rules: {
-    'ember/no-jquery': 'error'
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            legacyDecorators: true,
+        },
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:ember/recommended',
+        '@centerforopenscience/eslint-config/ember',
+    ],
+    plugins: [
+        '@typescript-eslint',
+    ],
+    env: {
+        browser: true,
+    },
+    rules: {
+        'ember/no-jquery': 'error',
+        'no-dupe-class-members': 'off',
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
 
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
-    }
-  ]
+        'import/export': 'off',
+        'import/named': 'off',
+        'import/no-unresolved': 'off',
+    },
+    overrides: [
+    // node files
+        {
+            files: [
+                '.eslintrc.js',
+                '.template-lintrc.js',
+                'ember-cli-build.js',
+                'testem.js',
+                'blueprints/*/index.js',
+                'config/**/*.js',
+                'lib/*/index.js',
+                'server/**/*.js',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+            env: {
+                browser: false,
+                node: true,
+            },
+            plugins: ['node'],
+            rules: {
+                ...require('eslint-plugin-node').configs.recommended.rules, // add your custom rules and overrides for node files here
+
+                // this can be removed once the following is fixed
+                // https://github.com/mysticatea/eslint-plugin-node/issues/77
+                'node/no-unpublished-require': 'off',
+            },
+        },
+    ],
 };
